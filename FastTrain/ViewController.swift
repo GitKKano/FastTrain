e3d31d8b8d3519085e4b4444eacb2a4b81467bb7
KKano <GitKKano@users.noreply.github.com>
2017-08-09 22:07:21 +0900

    New

---
+++ FastTrain/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FastTrain
+//
+//  Created by Kankuro Kano on 2017/08/09.
+//  Copyright © 2017 Kankuro Kano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
