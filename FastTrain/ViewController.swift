ce1e628d38245e1e28babba63265036f5fc466bb
KKano <GitKKano@users.noreply.github.com>
2017-08-10 22:19:16 +0900

    Added the Text

--- FastTrain/ViewController.swift
+++ FastTrain/ViewController.swift
@@ -14,10 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var trainName: UITextField!
     @IBOutlet weak var traffic: UISegmentedControl!
     @IBOutlet weak var placeInTrain: UISegmentedControl!
+    @IBOutlet weak var SuggetionN: UILabel!
+    @IBOutlet weak var Suggetion21: UILabel!
+    @IBOutlet weak var Suggetion22: UILabel!
+    @IBOutlet weak var test: UILabel!
+
+
+    
+
     var Data:Data?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         
     }
 
@@ -51,7 +60,7 @@ class ViewController: UIViewController {
                 //Sets Place in train
         var trafficINT = 0
         
-        
+
         
         UserDefaults.standard.set(10, forKey: "LukesAge") // Store
         UserDefaults.standard.integer(forKey: "LukesAge") // Retrieve
@@ -79,14 +88,9 @@ class ViewController: UIViewController {
         var placeInTrainF:Int = UserDefaults.standard.integer(forKey: "\(trainName)TrafficF")
         var placeInTrainM:Int = UserDefaults.standard.integer(forKey: "\(trainName)TrafficM")
         var placeInTrainB:Int = UserDefaults.standard.integer(forKey: "\(trainName)TrafficB")
-        var Num:Int = 0
+        var Num:Int = UserDefaults.standard.integer(forKey: "\(trainName)PlaceInTrainNUM")
         
         
-        
-        Num += 1
-        UserDefaults.standard.set(0, forKey: "\(trainName)TrafficF")
-        UserDefaults.standard.set(0, forKey: "\(trainName)TrafficM")
-        UserDefaults.standard.set(0, forKey: "\(trainName)TrafficB")
 
 
 //                print(trainName.text ?? "Toukaido Line", trafficINT, placaInTrainINT)
@@ -94,7 +98,10 @@ class ViewController: UIViewController {
         if placaInTrainINT == 1 {
             if trafficINT == 1 {
                 placeInTrainF += 1
+                Num += 1
+                
             } else {
+                Num += 1
                 
             }
         }
@@ -102,7 +109,10 @@ class ViewController: UIViewController {
         if placaInTrainINT == 2 {
             if trafficINT == 1 {
                 placeInTrainM += 1
+                Num += 1
+                
             } else {
+                Num += 1
                 
             }
         }
@@ -110,16 +120,20 @@ class ViewController: UIViewController {
         if placaInTrainINT == 3 {
             if trafficINT == 1 {
                 placeInTrainB += 1
+                Num += 1
+                
             } else {
+                Num += 1
                 
             }
         }
                 
             
+
         
         
-        
-        UserDefaults.standard.set(placaInTrainINT, forKey: "\(trainName)PlaceInTrain\(Num)")
+        UserDefaults.standard.set(Num, forKey: "\(trainName)PlaceInTrainNUM")
+
         UserDefaults.standard.set(placeInTrainF, forKey: "\(trainName)TrafficF")
         UserDefaults.standard.set(placeInTrainM, forKey: "\(trainName)TrafficM")
         UserDefaults.standard.set(placeInTrainB, forKey: "\(trainName)TrafficB")
@@ -127,30 +141,64 @@ class ViewController: UIViewController {
         print(UserDefaults.standard.integer(forKey: "\(trainName)TrafficF"))
         print(UserDefaults.standard.integer(forKey: "\(trainName)TrafficM"))
         print(UserDefaults.standard.integer(forKey: "\(trainName)TrafficB"))
+        test.text = "\(UserDefaults.standard.integer(forKey: "\(trainName)TrafficF")), \(UserDefaults.standard.integer(forKey: "\(trainName)TrafficM")), \(UserDefaults.standard.integer(forKey: "\(trainName)TrafficB"))"
 
         if UserDefaults.standard.integer(forKey: "\(trainName)TrafficF") > UserDefaults.standard.integer(forKey: "\(trainName)TrafficM") {
             if UserDefaults.standard.integer(forKey: "\(trainName)TrafficF") > UserDefaults.standard.integer(forKey: "\(trainName)TrafficB") {
                 // Ride FRONT
                 print(placeInTrainF)
                 print("#1")
-                
+                if UserDefaults.standard.integer(forKey: "\(trainName)PlaceInTrainNUM") > 2 {
+                    SuggetionN.text = "Recommended to ride in FRONT"
+                    Suggetion21.text = ""
+                    Suggetion22.text = ""
+                } else {
+                    SuggetionN.text = "Recommended to ride in FRONT"
+                    Suggetion21.text = "You have only submitted once."
+                    Suggetion22.text = "Try going to the middle or the back."
+                }
             } else {
                 //Ride BACK
                 print(placeInTrainB)
                 print("#2")
-                
+                if UserDefaults.standard.integer(forKey: "\(trainName)PlaceInTrainNUM") > 2 {
+                    SuggetionN.text = "Recommended to ride in BACK"
+                    Suggetion21.text = ""
+                    Suggetion22.text = ""
+                } else {
+                    SuggetionN.text = "Recommended to ride in BACK"
+                    Suggetion21.text = "You have only submitted once."
+                    Suggetion22.text = "Try going to the front or the middle."
+                }
             }
         } else {
             if UserDefaults.standard.integer(forKey: "\(trainName)TrafficM") > UserDefaults.standard.integer(forKey: "\(trainName)TrafficB") {
                 //Ride MIDDLE
                 print(placeInTrainM)
                 print("#3")
-                
+                if UserDefaults.standard.integer(forKey: "\(trainName)PlaceInTrainNUM") > 2 {
+                    SuggetionN.text = "Recommended to ride in MIDDLE"
+                    Suggetion21.text = ""
+                    Suggetion22.text = ""
+                } else {
+                    SuggetionN.text = "Recommended to ride in MIDDLE"
+                    Suggetion21.text = "You have only submitted once."
+                    Suggetion22.text = " Try going to the front or the back."
+                    
+                }
             } else {
                 //Ride BACK
                 print(placeInTrainB)
                 print("#4")
-                
+                if UserDefaults.standard.integer(forKey: "\(trainName)PlaceInTrainNUM") > 2 {
+                    SuggetionN.text = "Recommended to ride in BACK"
+                    Suggetion21.text = ""
+                    Suggetion22.text = ""
+                } else {
+                    SuggetionN.text = "Recommended to ride in BACK"
+                    Suggetion21.text = "You have only submitted once."
+                    Suggetion22.text = "Try going to the front or the middle."
+                }
             }
         }
         
